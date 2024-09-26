957de1500b4699a471b7b50b4d75774b7b9de9f1
Hun Kim <hun.kim.dev@gmail.com>
2024-09-26 23:53:13 +0900

    made medical wallet register logics

---
+++ src/medicalwallet/swagger/swagger.metadata.ts
@@ -0,0 +1,18 @@
+// // swagger.metadata.ts
+// import { BAD_REQUEST, INTERNAL_SERVER_ERROR } from 'src/common/exception/types';
+// import { createBody, createSwaggerOptions } from '../../common/swagger/swagger.decorator';
+// import { SUCCESS_RES } from '../types';
+
+
+// export const checkUsePipeOpts = createSwaggerOptions({
+//   summary: 'for testing nestJS Decorator',
+//   queries: [
+//     { name: 'param1', required: true, type: Number, description: 'The first parameter, must be a number' },
+//     { name: 'param2', required: false, type: String, description: 'The second parameter, optional string' }
+// ],
+//   responses: [  
+//     { status: 200, description: "request success, return param1", schema: { example: createBody(SUCCESS_RES, { param: 0 }) } },
+//     { status: 400, description: 'Bad Request', schema: { example: createBody(BAD_REQUEST, { message: "param1 must be a number conforming to the specified constraints, param1 should not be empty" }) } },
+//     { status: 500, description: 'Internal Server Error', schema: { example: createBody(INTERNAL_SERVER_ERROR, { message: "Internal server error" }) } }
+//   ]
+// });
